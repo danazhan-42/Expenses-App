dd941453a3c8f1d985cfca5cb862315f2d4695ab
dana <danazyt2016@gmail.com>
2023-10-10 13:12:10 -0700

    Implemented two screens and simple navigation between them, also configured the header style

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+
+import AllExpensesScreen from "./screens/AllExpensesScreen";
+import OverbudgetScreen from "./screens/OverbudgetScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="All Expenses"
+        screenOptions={{
+          headerStyle: { backgroundColor: "#483d8b" },
+          headerTintColor: "#fff",
+          headerTitleStyle: { fontWeight: "bold" },
+        }}
+      >
+        {/* Each screen component in your app is provided with the navigation prop automatically. */}
+        <Stack.Screen
+          name="All Expenses"
+          component={AllExpensesScreen}
+          options={{
+            title: "All Expenses",
+          }}
+        />
+        <Stack.Screen name="Overbudget Expenses" component={OverbudgetScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ screens/AllExpensesScreen.js
@@ -0,0 +1,21 @@
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import React from "react";
+
+export default function AllExpensesScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>AllExpensesScreen</Text>
+      <Pressable onPress={() => navigation.navigate("Overbudget Expenses")}>
+        <Text>Go to Overbudget</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
