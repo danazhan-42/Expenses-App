2c2c639fa1eaf93c5461f748312b6354b3b1faea
dana <danazyt2016@gmail.com>
2023-10-10 18:24:13 -0700

    Customized the tabs

--- App.js
+++ App.js
@@ -4,30 +4,34 @@ import { NavigationContainer } from "@react-navigation/native";
 
 import AllExpensesScreen from "./screens/AllExpensesScreen";
 import OverbudgetScreen from "./screens/OverbudgetScreen";
+import TabNavigator from "./src/navigations/TabNavigator";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
+    // <NavigationContainer>
+    //   <Stack.Navigator
+    //     initialRouteName="All Expenses"
+    //     screenOptions={{
+    //       headerStyle: { backgroundColor: "#483d8b" },
+    //       headerTintColor: "#fff",
+    //       headerTitleStyle: { fontWeight: "bold" },
+    //     }}
+    //   >
+    //     {/* Each screen component in your app is provided with the navigation prop automatically. */}
+    //     <Stack.Screen
+    //       name="All Expenses"
+    //       component={AllExpensesScreen}
+    //       options={{
+    //         title: "All Expenses",
+    //       }}
+    //     />
+    //     <Stack.Screen name="Overbudget Expenses" component={OverbudgetScreen} />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="All Expenses"
-        screenOptions={{
-          headerStyle: { backgroundColor: "#483d8b" },
-          headerTintColor: "#fff",
-          headerTitleStyle: { fontWeight: "bold" },
-        }}
-      >
-        {/* Each screen component in your app is provided with the navigation prop automatically. */}
-        <Stack.Screen
-          name="All Expenses"
-          component={AllExpensesScreen}
-          options={{
-            title: "All Expenses",
-          }}
-        />
-        <Stack.Screen name="Overbudget Expenses" component={OverbudgetScreen} />
-      </Stack.Navigator>
+      <TabNavigator />
     </NavigationContainer>
   );
 }
--- src/navigations/TabNavigator.js
+++ src/navigations/TabNavigator.js
@@ -2,22 +2,22 @@ import { StyleSheet } from "react-native";
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import AllExpensesScreen from "./src/screens/AllExpensesScreen";
-import OverbudgetScreen from "./src/screens/OverbudgetScreen";
+import AllExpensesScreen from "../../screens/AllExpensesScreen";
+import OverbudgetScreen from "../../screens/OverbudgetScreen";
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigator() {
   return (
-    <Tab.Navigator tabBarOptions={{ activeBackgroundColor: "tomato" }}>
+    <Tab.Navigator
+      tabBarOptions={{
+        activeBackgroundColor: "#483d8b",
+        activeTintColor: "gold",
+        inactiveBackgroundColor: "#483d8b",
+      }}
+    >
       <Tab.Screen name="All Expenses" component={AllExpensesScreen} />
       <Tab.Screen name="Overbudget" component={OverbudgetScreen} />
     </Tab.Navigator>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    activeBackgroundColor: "#483d8b",
-  },
-});
