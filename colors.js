66571cefb3cd4d55b892b246a02849d58708f696
dana <danazyt2016@gmail.com>
2023-10-21 10:48:55 -0700

    Implemented the basic components in AddExpense Screen

--- colors.js
+++ colors.js
@@ -1,3 +1,4 @@
 export const colors = {
   primary: "#483d8b",
+  secondary: "#EBE6EE",
 };
--- src/screens/AddExpenseScreen.js
+++ src/screens/AddExpenseScreen.js
@@ -1,10 +1,57 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+import React, { useState } from "react";
+import { colors } from "../../colors";
 
 export default function AddExpenseScreen({ navigation }) {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [quantity, setQuantity] = useState("");
+
   return (
-    <View>
-      <Text>AddExpenseScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>item *</Text>
+      <TextInput style={styles.input} value={name} onChangeText={setName} />
+      <Text style={styles.text}>Unit Price*</Text>
+      <TextInput
+        style={styles.input}
+        value={price}
+        onChangeText={setPrice}
+        keyboardType="numeric"
+      />
+      <Text style={styles.text}>Quantity*</Text>
+      <TextInput
+        style={styles.input}
+        value={quantity}
+        onChangeText={setQuantity}
+        keyboardType="numeric"
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.secondary,
+    alignItems: "center",
+    paddingTop: 50,
+    paddingHorizontal: 10,
+  },
+  text: {
+    color: colors.primary,
+    fontSize: 16,
+    fontWeight: "bold",
+    margin: 5,
+    marginLeft: 20,
+    alignSelf: "flex-start",
+  },
+  input: {
+    width: "90%",
+    color: colors.primary,
+    fontSize: 18,
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    padding: 5,
+    marginBottom: 10,
+  },
+});
