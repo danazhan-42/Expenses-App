c797fe12ae4e54b0bb598d5a64bc07a319d0b1f6
dana <danazyt2016@gmail.com>
2023-10-24 14:43:09 -0700

    Implemented the conditional rendering for the delete icon in edit screen

--- src/navigations/AppNavigator.js
+++ src/navigations/AppNavigator.js
@@ -1,15 +1,28 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Pressable, StyleSheet } from "react-native";
+import { StyleSheet, Alert } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 
 import AddExpenseScreen from "../screens/AddExpenseScreen";
 import TabNavigator from "./TabNavigator";
 import { AntDesign } from "@expo/vector-icons";
 import { deleteToDB } from "../firebase/firebaseHelper";
+import PressableButton from "../components/PressableButton";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const handleDelete = ({ route, navigation }) => {
+    Alert.alert("Important", "Are you sure you want to delete it?", [
+      { text: "Cancel", style: "cancel" },
+      {
+        text: "Yes",
+        onPress: () => {
+          deleteToDB(route.params.entry.id);
+          navigation.goBack();
+        },
+      },
+    ]);
+  };
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -31,24 +44,24 @@ export default function App() {
           options={({ route, navigation }) => ({
             title:
               route.params && route.params.entry ? "Edit" : "Add an Expense",
-            headerRight: () => (
-              <Pressable
-                style={{ paddingRight: 10 }}
-                onPress={() => {
-                  deleteToDB(route.params.entry.id);
-                  navigation.goBack();
-                }}
-              >
-                <AntDesign name="delete" size={20} color="white" />
-              </Pressable>
-            ),
+            headerRight:
+              route.params && route.params.entry
+                ? () => (
+                    <PressableButton
+                      pressedFunction={() =>
+                        handleDelete({ route, navigation })
+                      }
+                      defaultStyle={{ paddingRight: 10 }}
+                      pressedStyle={{ opacity: 0.8 }}
+                    >
+                      <AntDesign name="delete" size={20} color="white" />
+                    </PressableButton>
+                  )
+                : null,
           })}
         />
       </Stack.Navigator>
     </NavigationContainer>
-    // <View style={styles.container}>
-    //   <EntryItem entry={entry} />
-    // </View>
   );
 }
 
